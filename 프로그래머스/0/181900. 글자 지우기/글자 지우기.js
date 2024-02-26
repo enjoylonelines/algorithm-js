93b15ca79fff5ab5d1dd6e1b28e52e4bbf5ef89b
b4ur2old <86273335+enjoylonelines@users.noreply.github.com>
2024-02-26 20:04:28 +0900

    [level 0] Title: 글자 지우기, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181900. 글자 지우기/글자 지우기.js
+++ 프로그래머스/0/181900. 글자 지우기/글자 지우기.js
@@ -1,8 +1,8 @@
-function solution (s, d) {
-    return [...s].reduce((ans, cur, idx)=> (ans += d.includes(idx)? '' : cur), '');
+function solution (str, del) {
+    return [...str].reduce((ans, cur, idx)=> (ans += del.includes(idx)? '' : cur), '');
     // ans: 문자열을 저장할 인자, 초기값은 빈문자열
     // cur: 현재 문자열
     // idx: 현재 문자열 인덱스
-    // d 배열에 포함된 인덱스는 빈문자열을 반환하고 아니면 현재 문자열을 반환
-    // ans에는 d에 포함되지 않는 인덱스 값만 추가됨
+    // del 배열에 포함된 인덱스는 빈문자열을 반환하고 아니면 현재 문자열을 반환
+    // ans에는 del에 포함되지 않는 인덱스 값만 추가됨
 }
