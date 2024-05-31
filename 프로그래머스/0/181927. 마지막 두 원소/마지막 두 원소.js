f3f6fe48f0b04c671104ffcbd768e534e72e8a57
b4ur2old <86273335+enjoylonelines@users.noreply.github.com>
2024-06-01 03:10:50 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.js
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.js
@@ -1,7 +1,4 @@
-function solution(num_list) {
-    const lastValue = num_list[num_list.length-1];
-    const prevValue = num_list[num_list.length-2];
-    if(lastValue > prevValue) num_list.push(lastValue - prevValue);
-    else num_list.push(lastValue * 2);
-    return num_list;
+function solution(arr) {
+    const [prev, last] = arr.slice(-2);
+    return [...arr, last > prev ? last - prev : last * 2];
 }
