cbd4cb2befbfa4a1ab5d480006c7cef1dc8133fc
b4ur2old <86273335+enjoylonelines@users.noreply.github.com>
2024-09-19 16:33:46 +0900

    [level 0] Title: 배열 조각하기, Time: 2.45 ms, Memory: 36.8 MB -BaekjoonHub

--- 프로그래머스/0/181893. 배열 조각하기/배열 조각하기.js
+++ 프로그래머스/0/181893. 배열 조각하기/배열 조각하기.js
@@ -1,26 +1,14 @@
 function solution(arr, query) {
     
-    query.forEach(q => {
-        if(q % 2 === 0) {
-            arr = arr.splice(q, arr.length - 1)
+    query.forEach((cur, idx) => {
+        if(idx % 2 === 1) {
+            arr = arr.slice(cur, arr.length)
         }
-        if(q % 2 === 1) {
-            arr = arr.splice(0, q + 1)
+        if(idx % 2 === 0) {
+            arr = arr.slice(0, cur + 1)
         }
     })
     return arr
+    
 }
 
-function solution(arr, query) {
-    for(let i = 0 ; i < query.length ; i ++) {
-        const cur = query[i]
-        
-        if(i%2 === 0) {
-            arr.splice(cur+1, arr.length -(cur+1))
-        } else {
-             arr.splice(0, cur)
-        }
-    }
-    
-    return arr
-}
