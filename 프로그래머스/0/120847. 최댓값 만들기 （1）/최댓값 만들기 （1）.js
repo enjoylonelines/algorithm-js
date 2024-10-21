a4fbea7fa91716f810a390b074f3439e35f5a088
b4ur2old <86273335+enjoylonelines@users.noreply.github.com>
2024-10-22 00:09:37 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.31 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.js
@@ -0,0 +1,11 @@
+function solution(numbers) {
+    let max = 0;
+    for(let i = 0; i < numbers.length; i++) {
+        let num1 = numbers[i];
+        for(let j = i + 1; j < numbers.length; j++) {
+            let num2 = numbers[j];
+            max = Math.max(max, num1 * num2)
+        }
+    }
+    return max
+}
