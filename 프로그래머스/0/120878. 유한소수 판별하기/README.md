9f5e971f2e4f64a11f502740192541464546e48e
b4ur2old <86273335+enjoylonelines@users.noreply.github.com>
2026-03-05 21:40:52 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.12 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120878. 유한소수 판별하기/README.md
+++ 프로그래머스/0/120878. 유한소수 판별하기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 유한소수 판별하기 - 120878 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120878?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120878) 
 
 ### 성능 요약
 
-메모리: 33.4 MB, 시간: 0.28 ms
+메모리: 33.5 MB, 시간: 0.12 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 03월 05일 14:52:28
+2026년 03월 05일 21:40:50
 
 ### 문제 설명
 
--- 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.js
+++ 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.js
@@ -2,25 +2,12 @@ const factors = []
 function solution(a, b) {
     const g = gcd(a, b);
     let mother = b / g;
-    
-    trial_division(mother);
-    
-    let len = factors.filter(n => ![2,5].includes(n)).length
-    
-    return len === 0 ? 1 : 2;
+    while(mother % 5 === 0) mother /= 5;
+    while(mother % 2 === 0) mother /= 2;
+    if(mother > 1) return 2;
+    else return 1;
 }
 
-function trial_division(n) {
-    let d = 2;
-    while(d * d <= n) {
-        while(n % d === 0) {
-            factors.push(d)
-            n /= d;
-        }
-        d++;
-    }
-    if(n > 1) factors.push(n)
-}
 
 function gcd(a, b) {
     while(b !== 0) {
