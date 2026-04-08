79e0740f3b49091e09cb5ab59c356293c983abd9
b4ur2old <86273335+enjoylonelines@users.noreply.github.com>
2026-04-08 18:38:16 +0900

    [level 2] Title: 기능개발, Time: 0.50 ms, Memory: 9.21 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/README.md
+++ 프로그래머스/2/42586. 기능개발/README.md
@@ -1,10 +1,10 @@
 # [level 2] 기능개발 - 42586 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586?language=python3) 
 
 ### 성능 요약
 
-메모리: 33.4 MB, 시간: 0.27 ms
+메모리: 9.21 MB, 시간: 0.50 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 25일 17:30:32
+2026년 04월 08일 18:38:13
 
 ### 문제 설명
 
---
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -0,0 +1,18 @@
+def solution(progresses, speeds):
+    answer = []
+    
+    while progresses:
+        for idx in range(len(progresses)):
+            progresses[idx] += speeds[idx]
+        
+        
+        count = 0
+        while progresses and progresses[0] >= 100:
+            progresses.pop(0)
+            speeds.pop(0)
+            count += 1
+        
+        if count > 0:
+            answer.append(count)
+    
+    return answer
