01a82eba0976d069bb3897280edd5d686e11f659
b4ur2old <86273335+enjoylonelines@users.noreply.github.com>
2026-04-11 16:59:29 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 62.48 ms, Memory: 33.1 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/README.md
+++ 프로그래머스/1/42576. 완주하지 못한 선수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 완주하지 못한 선수 - 42576 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576) 
 
 ### 성능 요약
 
-메모리: 25.2 MB, 시간: 81.49 ms
+메모리: 33.1 MB, 시간: 62.48 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 04월 08일 08:20:23
+2026년 04월 11일 16:59:26
 
 ### 문제 설명
 
--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,9 +1,10 @@
 def solution(participant, completion):
-    participant.sort()
-    completion.sort()
-
-    for p,c in zip(participant, completion):
-        if p != c:
-            return p
-       
-    return participant[-1]
+    answer = ''
+    dic = {}
+    for name in participant:
+        dic[name] = dic.get(name, 0) + 1
+    for name in completion:
+        dic[name] -= 1
+    answer = max(dic, key = lambda x: dic[x])
+    
+    return answer
