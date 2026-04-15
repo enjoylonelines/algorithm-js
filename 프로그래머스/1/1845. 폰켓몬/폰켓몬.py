02a20b4658b7e8708b928e27e2b3482781538878
b4ur2old <86273335+enjoylonelines@users.noreply.github.com>
2026-04-15 11:05:48 +0900

    [level 1] Title: 폰켓몬, Time: 0.79 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -1,7 +1,4 @@
 def solution(nums):
-    answer = 0
     size = len(nums) // 2
-    var = len(set(nums))
-    if var < size: answer = var
-    else: answer = size
-    return answer
+    poketmons = len(set(nums))
+    return min(size, poketmons)
