86d24c1eb8465dbe8e871aab5d9aa48507029b7f
b4ur2old <86273335+enjoylonelines@users.noreply.github.com>
2026-04-23 17:32:48 +0900

    [level 2] Title: 카펫, Time: 0.07 ms, Memory: 9.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -0,0 +1,8 @@
+def solution(brown, yellow):
+    size = brown + yellow
+    for h in range(3,  size//3 + 1):
+        if not size % h == 0: continue
+        w = size // h
+        if w < h: continue
+        if (w - 2)*(h - 2) == yellow: return [w, h]
+
