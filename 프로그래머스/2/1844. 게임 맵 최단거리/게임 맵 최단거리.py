7e0db7e692a3876a7454d57c6b3201949c168d48
b4ur2old <86273335+enjoylonelines@users.noreply.github.com>
2026-04-24 15:28:51 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 11.39 ms, Memory: 9.24 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,24 +1,27 @@
-import collections
+from collections import deque
+
 def solution(maps):
     n = len(maps)
     m = len(maps[0])
-    dist = [[0] * m for _ in range(n)]
-    dx = [1, 0, -1, 0]
-    dy = [0, 1, 0, -1]
-    q = collections.deque()
-    q.append((0,0))
-    dist[0][0] = 1
+
+    dx = [1, -1, 0, 0]
+    dy = [0, 0, 1, -1]
+
+    q = deque([(0, 0)])
+
     while q:
         x, y = q.popleft()
+
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
-            if nx >= n or nx < 0 or ny >= m or ny < 0: continue
-            if maps[nx][ny] == 0: continue
-            if dist[nx][ny] != 0: continue
-            q.append((nx,ny))
-            dist[nx][ny] = dist[x][y] + 1    
-                
-    answer = dist[n - 1][m - 1]
-    if answer == 0: return -1
-    else: return answer
+
+            if nx < 0 or nx >= n or ny < 0 or ny >= m:
+                continue
+            if maps[nx][ny] != 1:
+                continue
+
+            maps[nx][ny] = maps[x][y] + 1
+            q.append((nx, ny))
+
+    return maps[n - 1][m - 1] if maps[n - 1][m - 1] != 1 else -1
