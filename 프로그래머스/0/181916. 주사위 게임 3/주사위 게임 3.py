1bf9714e3d093c7a342742d8b5aa627b7829536d
b4ur2old <86273335+enjoylonelines@users.noreply.github.com>
2026-06-07 21:45:49 +0900

    [level 0] Title: 주사위 게임 3, Time: 0.03 ms, Memory: 11.6 MB -BaekjoonHub

--- 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.py
+++ 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.py
@@ -1,20 +1,25 @@
+from collections import Counter
+
 def solution(a, b, c, d):
-    dic = {}
-    arr = [a,b,c,d]
-    for i in arr:
-        dic[i] = dic.get(i,0) + 1
-    l = len(dic)
-    
-    result = sorted(dic.items(), key=lambda x: x[1], reverse=True)
-    values = sorted(list(set([v for k,v in result])), reverse=True)
-    keys = [k for k, v in result]
-    print(result, l, values)
-    if l == 1: return 1111 * keys[0]
-    if l == 4: return min(keys)
-    if l == 2: 
-        if len(values) == 1: return (keys[0] + keys[1]) * abs(keys[0] - keys[1])
-        else: return (keys[0] * 10 + keys[1])**2
-    if l == 3: return keys[1] * keys[2]
-        
-        
-    
+    counter = Counter([a, b, c, d])
+    items = sorted(counter.items(), key=lambda x: x[1], reverse=True)
+
+    if len(counter) == 1:
+        p = items[0][0]
+        return 1111 * p
+
+    if len(counter) == 4:
+        return min(a, b, c, d)
+
+    if len(counter) == 3:
+        q = items[1][0]
+        r = items[2][0]
+        return q * r
+
+    p, c1 = items[0]
+    q, c2 = items[1]
+
+    if c1 == 3:
+        return (10 * p + q) ** 2
+    else:
+        return (p + q) * abs(p - q)
