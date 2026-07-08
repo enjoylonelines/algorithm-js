c738162ae5dc807d83c8c5c3bddd66b8decf05df
b4ur2old <86273335+enjoylonelines@users.noreply.github.com>
2026-07-08 11:07:22 +0900

    [level 1] Title: 노란불 신호등, Time: 387.83 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/1/468371. 노란불 신호등/노란불 신호등.py
+++ 프로그래머스/1/468371. 노란불 신호등/노란불 신호등.py
@@ -1,19 +1,11 @@
 def solution(signals):
-    G = []
-    Y = []
-    R = []
-
-    for g, y, r in signals:
-        G.append(g)
-        Y.append(y)
-        R.append(r)
     t = 1
     while True:
         flag = 0
         exit = 0
-        for i in range(len(signals)):
-            range_y = t % (G[i] + Y[i] + R[i])
-            if G[i] <= range_y < G[i] + Y[i]: flag += 1
+        for g,y,r in signals:
+            range_y = t % (g + y + r)
+            if g <= range_y < g + y: flag += 1
             if range_y == 0: exit += 1
         if flag == len(signals): return t + 1
         if exit == len(signals) and flag != len(signals): return -1
