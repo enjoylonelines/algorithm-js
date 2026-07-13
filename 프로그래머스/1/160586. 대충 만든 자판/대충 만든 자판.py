da2fd25e97e326ccd690607eaa45ae04283f0cbd
b4ur2old <86273335+enjoylonelines@users.noreply.github.com>
2026-07-13 16:08:31 +0900

    [level 1] Title: 대충 만든 자판, Time: 192.07 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.py
+++ 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.py
@@ -1,20 +1,19 @@
 def solution(keymap, targets):
     answer = []
-    
+    min_cnt = {}
     for t in targets: # t = "ABCD"
         cnt = 0
         for c in t: # c = "A"
             exist = False
             idx = 101
-            for k in keymap: # k = "ABACD"
-                tmp = 101
-                if c in k: 
-                    exist = True
-                    tmp = k.index(c)
-                idx = min(idx, tmp)
+            for key in keymap: # k = "ABACD"
+                for i,k in enumerate(key): 
+                    if k == c:
+                        min_cnt[c] = min(min_cnt.get(c, 101), i + 1)
+                        exist = True
             if not exist: 
                 cnt = -1
                 break
-            else: cnt += idx + 1
+            else: cnt += min_cnt[c]
         answer.append(cnt)
     return answer
