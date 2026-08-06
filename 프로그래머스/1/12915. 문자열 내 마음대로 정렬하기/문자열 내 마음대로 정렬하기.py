b414a3f94f7ff90c0ab3735cb2e7e402359ca7ff
b4ur2old <86273335+enjoylonelines@users.noreply.github.com>
2026-08-06 16:30:42 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 0.01 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.py
+++ 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.py
@@ -1,10 +1,4 @@
 def solution(strings, n):
-    # 포함확인?이 뭐임, 부분 문자열 확인
-    # 문자, 인덱스 추출
-    # 수정 시엔 list
-    # 반례 2개..?
-    def cmp(a):
-        return (a[n], a)
-    strings.sort(key=cmp)
-    return strings
-    
+    strings = [s[n] + s for s in strings]
+    strings.sort()
+    return [s[1:] for s in strings]
