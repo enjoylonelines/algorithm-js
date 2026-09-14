0d8ed8048b26e5cd4cc6e6592d88bd9f0cc74f86
b4ur2old <86273335+enjoylonelines@users.noreply.github.com>
2026-09-14 15:19:21 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 43.97 ms, Memory: 26.5 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -1,8 +1,6 @@
 def solution(arr):
-    ans = [arr[0]]
-    idx = 0
-    for num in arr:
-       if num != ans[idx]:
-        ans.append(num)
-        idx += 1
-    return ans
+    st = []
+    for n in arr:
+        if not st or st[-1] != n:
+            st.append(n)
+    return st
